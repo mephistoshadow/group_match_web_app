55c5d5af1b9627b5cd390babf30b61875ac6c48b
mephistoshadow <siqi.yang@mail.utoronto.ca>
2020-02-21 03:31:01 -0500

    admin add student page

---
+++ src/react-components/admin/admin.js
@@ -0,0 +1,14 @@
+"use strict"
+const log = console.log
+let state = 0;
+
+
+
+function showpop() {
+	document.querySelector(".popup").style.display = "block";
+}
+
+
+function closepop() {
+	document.querySelector(".popup").style.display = "none";
+}
