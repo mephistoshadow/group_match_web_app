988e495d38559ff0265c3c56dc7798187c75d655
Jinjie Sun <jinjie.sun@mail.utoronto.ca>
2020-02-22 15:47:26 -0500

    Add side bar to course and user for Admin

--- src/react-components/admin/Courses/course.js
+++ src/react-components/admin/Courses/course.js
@@ -12,3 +12,32 @@ function showpop() {
 function closepop() {
 	document.querySelector(".popup").style.display = "none";
 }
+
+
+function hamburgerClick() {
+    if (state == 0) {
+        openHamburger()
+    } else if (state == 1){
+        closeHamburger()
+    }
+}
+
+function openHamburger() {
+    const hamburger = document.querySelector('.hamburger-toggle')
+    
+    document.querySelector(".hamburgerMenu").style.width = "120px";
+    document.querySelector(".collapsedMenu").style.marginLeft = "120px";
+    document.querySelector(".card").style.marginLeft = "300px";
+    hamburger.style.transform = "rotate(90deg)";
+    state = 1;
+}
+
+function closeHamburger() {
+    const hamburger = document.querySelector('.hamburger-toggle')
+    
+    document.querySelector(".hamburgerMenu").style.width = "0px";
+    document.querySelector(".collapsedMenu").style.marginLeft = "0px";
+    document.querySelector(".card").style.marginLeft = "150px";
+    hamburger.style.transform = "rotate(0deg)"
+    state = 0;
+}
