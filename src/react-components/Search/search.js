73f6bd9cf640320754d0a36f1e855e6d5b809af3
Ilona Suleimenova <ilona.suleimenova@mail.utoronto.ca>
2020-02-26 21:14:59 -0500

    Create Search page

---
+++ src/react-components/Search/search.js
@@ -0,0 +1,32 @@
+"use strict"
+const log = console.log
+let state = 0;
+
+
+
+function hamburgerClick() {
+    if (state == 0) {
+        openHamburger()
+    } else if (state == 1){
+        closeHamburger()
+    }
+    
+}
+
+function openHamburger() {
+    const hamburger = document.querySelector('.hamburger-toggle')
+    
+    document.querySelector("#hamburgerMenu").style.width = "120px";
+    document.querySelector("#collapsedMenu").style.marginLeft = "120px";
+    hamburger.style.transform = "rotate(90deg)";
+    state = 1;
+}
+
+function closeHamburger() {
+    const hamburger = document.querySelector('.hamburger-toggle')
+    
+    document.querySelector("#hamburgerMenu").style.width = "0px";
+    document.querySelector("#collapsedMenu").style.marginLeft = "0px";
+    hamburger.style.transform = "rotate(0deg)"
+    state = 0;
+}
