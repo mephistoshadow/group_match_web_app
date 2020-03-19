87a8f74ecf78d3538a8e41e08d96a82739652d54
Ilona Suleimenova <ilona.suleimenova@mail.utoronto.ca>
2020-03-19 01:30:14 -0400

    Create Post model

--- backend/models/post.js
+++ backend/models/post.js
@@ -1,15 +1,24 @@
 /* Course mongoose model */
 const mongoose = require('mongoose')
 
-const Post = mongoose.model('Post', {
-	CourseName: {
+const PostSchema = new mongoose.Schema({
+	courseCode: {
 		type: String,
 		required: true,
-		maxlength:3
+		uppercase: true,
+		maxlength: 6
 	},
-	Content :{
-		type:String
+	content :{
+		type: String,
+		required: true,
+		trim: true,
+		maxlength: 280
+	},
+	author: {
+		type: mongoose.Schema.Types.ObjectId,
+		required: true
 	}
 })
 
+const Post = mongoose.model('Post', PostSchema) 
 module.exports = { Post }
