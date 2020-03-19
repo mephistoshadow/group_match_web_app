23e2803487ccc37088a43b8253e091b6dfc39479
mephistoshadow <siqi.yang@mail.utoronto.ca>
2020-03-19 18:47:42 -0400

    add partial api for courses and students

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,86 @@ app.use(bodyParser.json())
 
 
 
-/*** API Routes below ************************************/
+////////////////////////////////////////////////student api
+// add the student to database
+app.post('/students', (req, res) => {
+	
+	const student = new Student({
+		username: req.body.username,
+		name: req.body.name,
+		year: req.body.year,
+		courses:req.body.courses,
+		email: req.body.email,
+		role: req.body.role
+	})
+
+	student.save().then((result) => {
+		res.send(result)
+	}, (error) => {
+		res.status(400).send(error)
+	})
+})
+
+// get all the students from database
+app.get('/students', (req, res) => {
+	Student.find({}).then((students) => {
+		res.send({ students }) 
+	}, (error) => {
+		res.status(500).send(error)
+	})
+})
+
+// get student by their id
+app.get('/students/:id', (req, res) => {
+	const thisId = req.params.id
+	Student.findById(thisId).then((student) => {
+		if (!student) {
+			res.status(404).send()
+		} else {
+			res.send({ student })
+		}
+	}).catch((error) => {
+		res.status(500).send(error)
+	})
+})
+
+// delete the student by their id
+app.delete('/students/:id', (req, res) => {
+	const thisId = req.params.id
+	if (!ObjectID.isValid(thisId)) {
+		return res.redirect("/error")
+	}
+	Student.findByIdAndRemove(thisId).then((student) => {
+		if (!student) {
+			res.status(404).send();
+		} else {
+			res.send({ student })
+		}
+	}).catch((error) => {
+		res.status(500).send(error)
+	})
+})
+
+
+////////////////////////////////////////////////courses Api
+app.post('/courses', (req, res) => {
+	
+	const course = new Course({
+		title: req.body.title,
+		courseCode:req.body.courseCode,
+		people:req.body.people
+	})
+
+	course.save().then((result) => {
+		res.send(result)
+	}, (error) => {
+		res.status(400).send(error)
+	})
+})
+
+
+
+
 
 
 
