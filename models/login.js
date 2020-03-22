02d3a2116f3a0ff7eec3db38f828175d357b8ca9
Ilona Suleimenova <ilona.suleimenova@mail.utoronto.ca>
2020-03-22 17:11:14 -0400

    Update login and student models

--- models/login.js
+++ models/login.js
@@ -1,15 +1,29 @@
 /* Course mongoose model */
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 const Login = mongoose.model('LogInfo', {
 	username: {
 		type: String,
 		required: true,
+		trim: true,
+		unique: true
+		minlength: 1
 	},
 	password: {
+		type: String,
+		required: true,
+		minlength: 6
 	},
-	id: {
-		type: String
+	email: {
+		type: String,
+		required: true,
+		trim: true,
+		unique: true,
+		validate: {
+			validator: validator.isEmail,
+			message: "Not a valid email"
+		}
 	}
 })
 
--- models/student.js
+++ models/student.js
@@ -5,22 +5,25 @@ const Student = mongoose.model('Student', {
 	username: {
 		type: String,
 		required: true,
+		trim: true,
+		unique: true
 		minlength: 1
 	},
 	firstName: {
 		type: String,
 		required: true,
-		minlength: 1,
-		trim: true
+		trim: true,
+		minlength: 1
 	},
 	lastName: {
 		type: String,
 		required: true,
-		minlength: 1,
-		trim: true
+		trim: true,
+		minlength: 1
 	},
 	year: {
 		type: Number,
+		minvalue: 1,
 		required: true
 	},
 	courses: [String],
